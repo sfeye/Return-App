c9cd5a1d526e441032dd467b535f1162351f0e24
Samuel Feye <37005498+sfeye@users.noreply.github.com>
2021-09-11 11:34:04 -0500

    Setup firestore & driver component (#2)
    * Expo Init
    
    * Setup firestore & create App driver component

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAxJ9C7EPRM9Z-FvHm57F6QdANKqKVFAVY",
+  authDomain: "return-app-d1928.firebaseapp.com",
+  projectId: "return-app-d1928",
+  storageBucket: "return-app-d1928.appspot.com",
+  messagingSenderId: "532611013345",
+  appId: "1:532611013345:web:8775ddc67a98cdc177f5a0",
+};
