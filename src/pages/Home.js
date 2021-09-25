f2ae075625a2aeb6617290d62077dd3119ca4eaf
Momodou <alieujallowsu@gmail.com>
2021-09-25 11:15:23 -0500

    map initialize

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,43 @@
 import React, { useState, useEffect } from "react";
+import firebase from "firebase";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  initializeUser,
+  updateUserLocation,
+} from "../store/actions/userActions";
 import { View, Text, StyleSheet, Keyboard, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Button, Input, Icon } from "react-native-elements";
 import * as Location from "expo-location";
 import MapView, { Marker } from "react-native-maps";
 
-const Home = ({ navigation }) => {
-  const [location, setLocation] = useState(null);
-  const [region, setRegion] = useState({
-    latitude: location.coords.latitude,
-    longitude: location.coords.longitude,
-    latitudeDelta: 0.05,
-    longitudeDelta: 0.05,
-  });
+const Home = ({ route, navigation }) => {
+  // --------- Redux ----------- //
+  const dispatch = useDispatch();
+  const userLocation = useSelector((state) => state.userReducer.location);
+  // --------------------------- //
+
+  // ------- Local State ------- //
   const [errorMsg, setErrorMsg] = useState(null);
+  const [region, setRegion] = useState({
+    latitude: userLocation.coords.latitude,
+    longitude: userLocation.coords.longitude,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  // --------------------------- //
 
   useEffect(() => {
+    firebase
+      .firestore()
+      .collection("users")
+      .where("email", "==", route.params.email)
+      .onSnapshot((snapshot) => {
+        snapshot.docs.map((doc) =>
+          dispatch(initializeUser(doc.id, doc.data()))
+        );
+      });
+
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
@@ -24,18 +46,15 @@ const Home = ({ navigation }) => {
       }
 
       let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
+      dispatch(updateUserLocation(location));
+      setRegion({
+        latitude: userLocation.coords.latitude,
+        longitude: userLocation.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
     })();
   }, []);
-
-  location
-    ? console.log(
-        "User location is: " +
-          location.coords.latitude +
-          " long: " +
-          location.coords.longitude
-      )
-    : console.log("error message: " + errorMsg);
 
   return (
     <View>
@@ -52,21 +71,12 @@ const Home = ({ navigation }) => {
         onPress={() => navigation.push("RequestScreen")}
       />
 
-      {location ? (
-        <MapView
-          style={{ flex: 1 }}
-          region={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.05,
-            longitudeDelta: 0.05,
-          }}
-          onRegionChange={setRegion}
-        >
+      {userLocation ? (
+        <MapView style={{ flex: 1 }} region={region} onRegionChange={setRegion}>
           <Marker
             coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
+              latitude: userLocation.coords.latitude,
+              longitude: userLocation.coords.longitude,
             }}
           />
         </MapView>
