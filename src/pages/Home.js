30137d809c6bd51bef29c38c8162b990212552c0
Momodou <alieujallowsu@gmail.com>
2021-09-25 11:07:45 -0500

    Auto stash before merge of "main" and "origin/main"

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,16 @@ import { View, Text, StyleSheet, Keyboard, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Button, Input, Icon } from "react-native-elements";
 import * as Location from "expo-location";
+import MapView, { Marker } from "react-native-maps";
 
 const Home = ({ navigation }) => {
   const [location, setLocation] = useState(null);
+  const [region, setRegion] = useState({
+    latitude: location.coords.latitude,
+    longitude: location.coords.longitude,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
+  });
   const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
@@ -21,13 +28,14 @@ const Home = ({ navigation }) => {
     })();
   }, []);
 
-  console.log(
-    "User location is: " +
-      location.coords.latitude +
-      " long: " +
-      location.coords.longitude
-  );
-  console.log("error message: " + errorMsg);
+  location
+    ? console.log(
+        "User location is: " +
+          location.coords.latitude +
+          " long: " +
+          location.coords.longitude
+      )
+    : console.log("error message: " + errorMsg);
 
   return (
     <View>
@@ -43,6 +51,28 @@ const Home = ({ navigation }) => {
         title="New Request ? "
         onPress={() => navigation.push("RequestScreen")}
       />
+
+      {location ? (
+        <MapView
+          style={{ flex: 1 }}
+          region={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05,
+          }}
+          onRegionChange={setRegion}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          />
+        </MapView>
+      ) : (
+        React.Fragment
+      )}
     </View>
   );
 };
