840eddf88252577270902b5d8b7aa962e5980450
Samuel Feye <samfeye@gmail.com>
2021-10-06 18:46:55 -0500

    🗺 map accessory pages ♿️

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Settings from "./pages/Settings";
+import Previous from "./pages/Previous";
+import Wallet from "./pages/Wallet";
 import RequestScreen from "./components/RequestScreen";
 import ForgotPassword from "./components/ForgotPassword";
 
@@ -42,37 +45,68 @@ export default function App() {
   const HomeStack = createStackNavigator();
   // --------------------------- //
 
+  // -------- Templates -------- //
+  function HomeStackTemplate(name, component, title, initialParams) {
+    return (
+      <HomeStack.Screen
+        name={name}
+        component={component}
+        options={{
+          title: title,
+          headerTitleStyle: {
+            fontSize: 15,
+            color: "white",
+          },
+          headerTintColor: "white",
+          headerStyle: {
+            backgroundColor: "blue",
+          },
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => firebase.auth().signOut()}
+              style={{ maginBottom: 10, marginRight: 20 }}
+            >
+              <Ionicons name={"power"} size={20} color={"white"} />
+            </TouchableOpacity>
+          ),
+        }}
+        initialParams={initialParams}
+      />
+    );
+  }
+  // --------------------------- //
+
   return (
     <Provider store={Store}>
       {true ? (
         <NavigationContainer>
           <HomeStack.Navigator>
-            <HomeStack.Screen
-              name="HomeStack"
-              component={Home}
-              options={{
-                title: "Welcome tester",
-                headerTitleAlign: "left",
-                headerTitleStyle: {
-                  fontSize: 15,
-                  color: "white"
-                },
-                headerTintColor: "white",
-                headerStyle: {
-                  backgroundColor: "blue"
-                },
-                headerRight: () => (
-                  <TouchableOpacity
-                    onPress={() => firebase.auth().signOut()}
-                    style={{ maginBottom: 10, marginRight: 20 }}
-                  >
-                    <Ionicons name={"power"} size={20} color={"white"} />
-                  </TouchableOpacity>
-                )
-              }}
-              initialParams={{ email: "email" }}
-            />
-            <HomeStack.Screen name="RequestScreen" component={RequestScreen} />
+            {HomeStackTemplate(
+              (name = "HomeStack"),
+              (component = Home),
+              (title = "Welcome tester"),
+              (initialParams = { email: "email" })
+            )}
+            {HomeStackTemplate(
+              (name = "RequestScreen"),
+              (component = RequestScreen),
+              (title = "Request a pickup")
+            )}
+            {HomeStackTemplate(
+              (name = "SettingsScreen"),
+              (component = Settings),
+              (title = "Profile Settings")
+            )}
+            {HomeStackTemplate(
+              (name = "WalletScreen"),
+              (component = Wallet),
+              (title = "Your Payments")
+            )}
+            {HomeStackTemplate(
+              (name = "PreviousScreen"),
+              (component = Previous),
+              (title = "Previous Jobs")
+            )}
           </HomeStack.Navigator>
         </NavigationContainer>
       ) : (
@@ -84,12 +118,12 @@ export default function App() {
               options={{
                 headerTitleStyle: {
                   color: "white",
-                  textAlign: "center"
+                  textAlign: "center",
                 },
                 headerTintColor: "white",
                 headerStyle: {
-                  backgroundColor: "blue"
-                }
+                  backgroundColor: "blue",
+                },
               }}
             />
             <AuthStack.Screen
@@ -98,12 +132,12 @@ export default function App() {
               options={{
                 headerTitleStyle: {
                   color: "white",
-                  textAlign: "center"
+                  textAlign: "center",
                 },
                 headerTintColor: "white",
                 headerStyle: {
-                  backgroundColor: "blue"
-                }
+                  backgroundColor: "blue",
+                },
               }}
             />
           </AuthStack.Navigator>
---
+++ src/pages/Previous.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Previous = () => {
+  return (
+    <View>
+      <Text>Previous</Text>
+    </View>
+  );
+};
+
+export default Previous;
+
+const styles = StyleSheet.create({});
