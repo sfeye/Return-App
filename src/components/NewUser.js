836ff6bdf0a13874eff7d74f23656072a09d0c65
lhorosz <59933515+lhorosz@users.noreply.github.com>
2021-11-06 08:37:02 -0500

    NewUser New Fields (#16)

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -6,16 +6,40 @@ import {
   TouchableOpacity,
   StyleSheet
 } from "react-native";
+import { CheckBox, Icon } from "react-native-elements";
 import firebase from "firebase";
 
 const NewUser = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [address, setAddress] = useState("");
+  const [billinginfo, setBillinginfo] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  // const [isValid, setIsvalid] = useState(false);
+  const [acceptedterms, setAcceptedterms] = useState(false);
 
-  const adduser = (email, password) => {
+  const adduser = (
+    email,
+    password,
+    name,
+    phone,
+    billinginfo,
+    address,
+    acceptedterms
+  ) => {
     firebase
       .auth()
-      .createUserWithEmailAndPassword(email, password)
+      .createUserWithEmailAndPassword(
+        email,
+        password //,
+        //name,
+        //phone,
+        //billinginfo,
+        //address,
+        // acceptedterms
+        // bug here can I use this to have more than email and password only
+      )
       .then(res => {
         // Signed in
         console.log(res);
@@ -29,12 +53,13 @@ const NewUser = ({ navigation }) => {
       .firestore()
       .collection("users")
       .add({
-        acceptedterms: true,
-        address: "",
-        billinginfo: "",
+        acceptedterms: acceptedterms,
+        address: address,
+        billinginfo: billinginfo,
         isValid: true,
-        name: "",
-        phone: ""
+        name: name,
+        phone: phone,
+        email: email
       })
       .then(() => {
         alert("Success");
@@ -67,9 +92,66 @@ const NewUser = ({ navigation }) => {
         onChangeText={setPassword}
       />
 
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        placeholder="Address"
+        placeholderTextColor="#0000FF"
+        autoCapitalize="none"
+        value={address}
+        onChangeText={setAddress}
+      />
+
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        placeholder="Billing Information"
+        placeholderTextColor="#0000FF"
+        autoCapitalize="none"
+        value={billinginfo}
+        onChangeText={setBillinginfo}
+      />
+
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        placeholder="First and Last Name"
+        placeholderTextColor="#0000FF"
+        autoCapitalize="none"
+        value={name}
+        onChangeText={setName}
+      />
+
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        placeholder="Phone Number"
+        placeholderTextColor="#0000FF"
+        autoCapitalize="none"
+        value={phone}
+        onChangeText={setPhone}
+      />
+
+      <CheckBox
+        center
+        title="Accept terms and conditions"
+        checked={acceptedterms}
+        onPress={() => setAcceptedterms(!acceptedterms)}
+      />
+
       <TouchableOpacity
         style={styles.submitButton}
-        onPress={() => adduser(email, password)}
+        onPress={() =>
+          adduser(
+            email,
+            password,
+            name,
+            phone,
+            billinginfo,
+            address,
+            acceptedterms
+          )
+        }
       >
         <Text style={styles.submitButtonText}> Submit </Text>
       </TouchableOpacity>
