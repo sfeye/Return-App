03c5d8623d487f16b448ecba3310f7600ba85317
Samuel Feye <samfeye@gmail.com>
2021-11-08 20:32:31 -0600

    custom payment screen - start.. not working

---
+++ src/config/stripe.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://expo-stripe-server-example.glitch.me";
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,113 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useStripe } from "@stripe/stripe-react-native";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Alert,
+  ScrollView,
+  StyleSheet,
+  Text,
+  Button,
+  View,
+} from "react-native";
+import { API_URL } from "../config/stripe";
 
 const Wallet = () => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [paymentSheetEnabled, setPaymentSheetEnabled] = useState(false);
+  const [loading, setLoadng] = useState(false);
+  const [clientSecret, setClientSecret] = useState();
+
+  const fetchPaymentSheetParams = async () => {
+    const response = await fetch(`${API_URL}/payment-sheet`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const { paymentIntent, ephemeralKey, customer } = await response.json();
+    setClientSecret(paymentIntent);
+    return {
+      paymentIntent,
+      ephemeralKey,
+      customer,
+    };
+  };
+
+  const openPaymentSheet = async () => {
+    if (!clientSecret) {
+      return;
+    }
+    setLoadng(true);
+    const { error } = await presentPaymentSheet({
+      clientSecret,
+    });
+    console.log(error);
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      Alert.alert("Success", "The payment was confirmed successfully");
+    }
+    setPaymentSheetEnabled(false);
+    setLoadng(false);
+  };
+
+  const initialisePaymentSheet = async () => {
+    const { paymentIntent, ephemeralKey, customer } =
+      await fetchPaymentSheetParams();
+
+    const { error } = await initPaymentSheet({
+      customerId: customer,
+      customerEphemeralKeySecret: ephemeralKey,
+      paymentIntentClientSecret: paymentIntent,
+      customFlow: false,
+      merchantDisplayName: "Example Inc.",
+      style: "alwaysDark",
+    });
+    if (!error) {
+      setPaymentSheetEnabled(true);
+    }
+  };
+
+  useEffect(() => {
+    // In your app’s checkout, make a network request to the backend and initialize PaymentSheet.
+    // To reduce loading time, make this request before the Checkout button is tapped, e.g. when the screen is loaded.
+    initialisePaymentSheet();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <View>
-      <Text>Wallet</Text>
-    </View>
+    <ScrollView
+      accessibilityLabel="payment-screen"
+      style={styles.container}
+      keyboardShouldPersistTaps="handled"
+    >
+      <Button
+        variant="primary"
+        loading={loading}
+        disabled={!paymentSheetEnabled}
+        title="Checkout"
+        onPress={openPaymentSheet}
+      />
+    </ScrollView>
   );
 };
 
 export default Wallet;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: 20,
+    paddingHorizontal: 16,
+  },
+  form: {
+    width: "100%",
+    height: 400,
+  },
+  buttonContainer: {
+    marginTop: 30,
+  },
+});
